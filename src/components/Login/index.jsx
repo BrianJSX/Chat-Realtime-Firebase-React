20b38b97c470c61d9d5ea1cf1920de1e1640b24a
OnionSmit <minhho.technology@gmail.com>
2021-09-02 14:30:27 +0700

    css page login

--- src/components/Login/index.jsx
+++ src/components/Login/index.jsx
@@ -8,32 +8,38 @@ import "./style.scss";
 const fbProvider = new firebase.auth.FacebookAuthProvider();
 
 function Login() {
-  const handleFbLogin = async  () => {
+  const handleFbLogin = async () => {
     const { user, additionalUserInfo } = await auth.signInWithPopup(fbProvider);
 
-    if(additionalUserInfo?.isNewUser) { 
-      addDocument('users', {
+    if (additionalUserInfo?.isNewUser) {
+      addDocument("users", {
         displayName: user.displayName,
         email: user.email,
         photoURL: user.photoURL,
         uid: user.uid,
-        providerId: additionalUserInfo.providerId
+        providerId: additionalUserInfo.providerId,
       });
     }
-
   };
 
   return (
-    <div>
+    <div className="container">
       <div className="login">
-        <div className="login__title">
-          <h1>Chat OlaHub</h1>
+        <div className="login-left">
+          <div className="login-left__name">OnionDev</div>
+          <div className="login-left__description">
+            OnionApp giúp bạn kết nối và chia sẻ với mọi người trong cuộc sống
+            của bạn.
+          </div>
         </div>
-        <div className="login__button">
-          <Button type="primary" onClick={handleFbLogin}>
-            Đăng Nhập Bằng Facebook
-          </Button>
-          <Button type="danger">Đăng Nhập Bằng Google</Button>
+        <div className="login-right">
+          <div className="login-right__button">
+            <div>
+              <Button type="primary" onClick={handleFbLogin}>
+                Đăng Nhập Bằng Facebook
+              </Button>
+            </div>
+          </div>
         </div>
       </div>
     </div>
